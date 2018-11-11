073bfbf764690d09c0a26e9b4ee723c9d25a433b
atrest890 <swarovski991@gmail.com>
2018-11-11 07:46:14 +0700

    Kramer method was added

---
+++ numerical_methods/Kramer/Kramer.py
@@ -0,0 +1,14 @@
+import numpy as np
+import copy
+
+mx_a = np.array([[1, 1, 0], [1, 2, 1], [0, 1, 2]])
+mx_b = np.array([2, 4, 3])
+detX = np.linalg.det(mx_a)
+print("Roots of the equation:")
+for i in range(3):
+    mx_i = copy.copy(mx_a)
+    mx_i[:,i] = mx_b
+    detXi = np.linalg.det(mx_i)
+    r = detXi / detX
+    print(r)
+    
