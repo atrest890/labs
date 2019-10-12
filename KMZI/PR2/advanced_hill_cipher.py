7164e55326dc0e8005b4ba08a7b810771caf7128
atrest890 <swarovski991@gmail.com>
2019-10-12 17:17:07 +0700

    Copied some function from hill cipher

--- KMZI/PR2/advanced_hill_cipher.py
+++ KMZI/PR2/advanced_hill_cipher.py
@@ -32,6 +32,29 @@ def getPlainMatrix(plaintext, key_size):
 
     return matrix
 
+def getMatrix(key):
+    n = int( m.sqrt( len(key) ) )
+    matrix = []
+
+    index1 = 0
+    index2 = n
+
+    while index1 != len(key):
+        matrix.append( [ ALPHABET.index(x) for x in key[index1 : index2] ] )
+        index1 = index2
+        index2 += n
+
+    return matrix
+
+def getMatrix2(key):
+    n = int( m.sqrt( len(key) ) )
+    matrix = []
+
+    for pos in range(0, n):
+        matrix.append( [ ALPHABET.index(x) for x in key[pos::n]] )
+
+    return matrix
+
 
 def getNewKey(key2, key1):
     return np.matmul(key2, key1)
@@ -41,15 +64,44 @@ def getNewInverseKey(inv_key1, inv_key2):
     return np.matmul(inv_key1, inv_key2)
 
 
+def getInverseKey(key):
+    inverseKey = Matrix(key).inv_mod(POWER)
+
+    return np.array(inverseKey, dtype=int)
+
+def hillEncrypt(textMatrix, keyMatrix):
+    encMatrix = np.matmul(textMatrix, keyMatrix)
+    encMatrix = np.remainder(encMatrix, POWER)
+
+    encText = ''
+    for row in encMatrix:
+        encText += ''.join( [ALPHABET[i] for i in row] )
+
+    return encText
+
+
+def hillDecrypt(text, key):
+    inverseKey = getInverseKey(key)
+    decMatrix = np.matmul(text, inverseKey)
+    decMatrix = np.remainder(decMatrix, POWER).flatten()
+    
+    return ''.join( [ALPHABET[i] for i in decMatrix] )
+
+
 while True:
     plaintext = input("Input your text: ")
-    key = input("Input your key: ")
+    key1 = input("Input your first key: ")
+    key2 = input("Input your second key: ")
 
     textMatrix = np.array(getPlainMatrix(plaintext, len(key)))
     print("Plain text matrix is\n", textMatrix)
 
-    keyMatrix = np.array(getMatrix2(key))
-    print("\nKey matrix is\n", keyMatrix)
+    keyMatrix1 = np.array(getMatrix2(key1))
+    print("\nFirst key matrix is\n", keyMatrix1)
+
+    keyMatrix2 = np.array(getMatrix2(key2))
+    print("\nSecond key matrix is\n", keyMatrix2)
+
 
     if keyMatrix.shape[0] != keyMatrix.shape[1]:
         print("\nError: key must be square matrix\n")
