2b3c73ba176cdb89db9c77559f6032e6db8dd449
atrest890 <swarovski991@gmail.com>
2019-10-19 15:43:52 +0700

    code: add encrypt function

--- KMZI/PR3/vigenere_autokey.py
+++ KMZI/PR3/vigenere_autokey.py
@@ -18,4 +18,14 @@ def tabula_recta():
 def autokey(text, symbol):
     return symbol + text[:len(text)-1]
 
+def encrypt(text, key):
+    square = tabula_recta()
+    new_text = ''
+    for i in range(0, len(text)):
+        n = ALPHABET.index(key[i])
+        m = ALPHABET.index(text[i])
+        new_text += square[n][m]
+
+    return new_text
+
 
